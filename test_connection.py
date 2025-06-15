533ca789b58dc41714cf92ccb73016ecc550dd4f
Clayton Wells <claytonwells@Mac.home.local>
2025-06-14 21:14:03 -0500

    Add test connection script

--- test_connection.py
+++ test_connection.py
@@ -1,12 +1,17 @@
 import os
+import ssl
 import sqlalchemy
 
+ssl_context = ssl.create_default_context()
+
 try:
-    engine = sqlalchemy.create_engine(os.environ["DATABASE_URL"], connect_args={"sslmode": "require"})
+    engine = sqlalchemy.create_engine(
+        os.environ["DATABASE_URL"],
+        connect_args={"ssl_context": ssl_context}
+    )
     with engine.connect() as conn:
         print("✅ Connected successfully!")
 except Exception as e:
     print("❌ Connection failed:")
     print(e)
     exit(1)
-
